00ebc17a143a689dc053426c9a2aa438cda5a47c
Akash Behera <kumarakash05534@gmail.com>
2025-02-12 17:15:28 +0530

    Update LoginPage.jsx

--- HomeRentalClient/src/pages/LoginPage.jsx
+++ HomeRentalClient/src/pages/LoginPage.jsx
@@ -16,7 +16,7 @@ const LoginPage = () => {
     e.preventDefault()
 
     try {
-      const response = await fetch ("http://localhost:3001/auth/login", {
+      const response = await fetch ("https://homyrentalserver2.onrender.com/auth/login", {
         method: "POST",
         headers: {
           "content-type": "application/json"
