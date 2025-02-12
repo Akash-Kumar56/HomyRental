cb114732acf971e0a089a3f130c5eccc729d16d8
Akash-Kumar56 <kumarakash05534@gmail.com>
2025-02-12 18:55:24 +0530

    Updated ListingCard.scss to use @use instead of @import

--- HomeRentalClient/src/components/ListingCard.jsx
+++ HomeRentalClient/src/components/ListingCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../styles/Listingcard.scss'
+import '../styles/ListingCard.scss'
 import { ArrowForwardIos, ArrowBackIos, Favorite } from '@mui/icons-material'
 import { useState } from 'react'
 import { useNavigate } from "react-router-dom";
